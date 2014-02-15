4334352c11e272fc11b21309740f89cfd19056e4
Monica <monicabdimitrova@gmail.com>
2014-02-15 14:58:25 +0200

    Add initializing of notes table in Database

---
+++ database/database_notes.rb
@@ -0,0 +1,12 @@
+require 'sequel'
+
+DB = Sequel.sqlite "db.sqlite"
+
+DB.create_table :notes do
+  primary_key :id
+  foreign_key (:user_id, :users)
+  String :title
+  String :text
+  String :tags
+  String :notebook
+end
